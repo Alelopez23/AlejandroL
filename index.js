53e65e14ef490ebeb8b0e87d6ab86ff15a0ff2f1
Alejandro <aleariellopez@gmail.com>
2022-06-28 23:42:32 -0300

    entrega3

--- index.js
+++ index.js
@@ -2,36 +2,29 @@ function saludar() {
 let nombre = prompt("Hola!! ¿Cual es tu nombre?")
 alert("Bienvenidos al consultorio DR.LOPEZ-" + nombre )
 }
-function medicos() {
-    let nombre = prompt("Con que medico quiere ser atendido (Hernandez - Ramirez)")
-    if (nombre == "Hernandez") {
-        alert("Va a ser atendido por el DR. Hernandez" )
-        return
+function medicos() { 
+    let listaMedicos = [ "Paz" , "Lio" ] 
+    let nombre = prompt("Con que medico quiere ser atendido " + listaMedicos.map((medico)=>  "Dr " + medico ))
+    if (nombre == "Paz") {
+        alert("Va a ser atendido por el DR. Paz" )
+        return nombre
     } 
 
-    if (nombre == "Ramirez") {
-        alert("Va a ser atendido por el DR. Ramirez" )
-        return
+    if (nombre == "Lio") {
+        alert("Va a ser atendido por el DR. Lio" )
+        return nombre
     } 
     alert("Escribio mal el nombre del Doctor, vuelva a intentar"  )
     medicos ()
+
     }
 
-    function turnos() {
-        let nombre = prompt("Atención del DR. Hernandez los dias LUN - JUE - SAB ok?")
-        if (nombre == "ok") {
-            alert("Elija entre LUNES - JUEVES - SABADO " )
-            alert("LUNES 10am - JUEVES 9am - SABADO 12am" )
-            return
+function turnos( medico ) {
+  let eleccion = prompt("Elija entre LUNES 10am - JUEVES 9am - SABADO 12am")  
+  alert ( "Tiene cita con el Dr " + medico + " el dia " + eleccion )
+
+}
 
 
-        } 
 
-        alert("Escribio mal el nombre del Doctor, vuelva a intentar"  )
-        medicos ()
-        }
-    
-
-saludar()
-medicos()
-turnos ()
+turnos (medicos(saludar()))
